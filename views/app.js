d3a540c9158ce46fba4a35072699f6b510613d70
Kaizyru <38268496+Kaizyru@users.noreply.github.com>
2019-08-07 09:21:01 +0500

    new

---
+++ views/app.js
@@ -0,0 +1,8 @@
+const { shell} = require('electron')
+
+document.addEventListener('click', function (event) {
+  if (event.target.tagName === 'A' && event.target.href.startsWith('http')) {
+    event.preventDefault()
+    shell.openExternal(event.target.href)
+  }
+})
